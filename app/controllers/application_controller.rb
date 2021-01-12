fa0891e9960700b426427b2e6f83b8fb24729c1f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-12 18:37:22 -0500

    logged in/current user helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
 
-  def logged_in?
-    !!current_user
-  end
+    def logged_in?
+      !!current_user
+    end
+
+    def is_current_user?(user)
+      user == current_user
+    end
+
+    def redirect_if_not_logged_in
+      redirect_to login_path if !logged_in?
+      flash[:message] =  "You must log in to view that page."
+    end
+
+    def redirect_if_logged_in
+      redirect_to user_path(current_user) if logged_in?
+    end
 end
