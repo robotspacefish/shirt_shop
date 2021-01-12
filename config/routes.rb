b9e0ab1a299858f474cfee66c0d2229a87ea1c79
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-12 18:36:58 -0500

    nest shops in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :shops
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show] do
+    resources :shops, only: [:show]
+  end
 end
