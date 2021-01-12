171094c825cc24c5e3442a07b945d5779ea27aea
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-12 14:02:38 -0500

    session login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
