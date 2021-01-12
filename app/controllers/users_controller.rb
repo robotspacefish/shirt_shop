288dd43485580c2be3494d73fc4e4f7abb88f7a9
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-12 13:50:56 -0500

    user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :shop_owner)
+    end
 end
