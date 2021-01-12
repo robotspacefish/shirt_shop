af08f59fcce693b22846e4f154382a17a3a7b558
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-12 14:56:23 -0500

    add invalid credentials msg

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
+      flash[:message] = "Invalid credentials"
       redirect_to login_path
     end
   end
