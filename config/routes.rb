fe2cd6a8b0c3fe04cd54388bf12f4676ef1c45b4
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-12 22:14:17 -0500

    shop & shirt routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
   resources :users, only: [:show] do
     resources :shops, only: [:show]
   end
+
+  resources :shops do
+    resources :shirts, shallow: true
+  end
+
+  resources :shirts
 end
